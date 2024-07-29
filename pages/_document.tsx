1dfececbac381dfa497675b334de7259debe7e2b
Samrat Mukherjee <samrat.mukherjee2022@gmail.com>
2024-07-29 13:06:56 +0530

    microsoft clarity added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -36,7 +36,7 @@ export default function Document() {
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
         </Script>
 
-        <Script type="text/javascript">
+        <Script id="microsoft-script" type="text/javascript">
           {` (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
