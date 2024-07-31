f9aaed045deaf441b31c19ff9fd2172aa2af3760
Samrat Mukherjee <samrat.mukherjee2022@gmail.com>
2024-07-31 12:34:47 +0530

    microsoft clarity head added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,15 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+      <Script id="microsoft-script" type="text/javascript">
+          {` (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "netrg2hrj8");`}
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
@@ -36,13 +44,7 @@ export default function Document() {
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
         </Script>
 
-        <Script id="microsoft-script" type="text/javascript">
-          {` (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "netrg2hrj8");`}
-        </Script>
+        
       </body>
     </Html>
   );
