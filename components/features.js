180b791907b4fabddb5c63fba5861d01b2d70933
shrutakirti <chakrabortyshrutakirti@gmail.com>
2024-10-25 18:14:26 +0530

    added feature UI

--- components/features.js
+++ components/features.js
@@ -1,61 +1,29 @@
 import React from "react";
 import styles from "../styles/feature.module.css";
-import Image from "next/image";
-import BentoGrid from "./magicui/bentoGrid";
-import BentoCard from "./magicui/bentoCard";
 
-
-const features = [
-  {
-    name: "Save your files",
-    description: "We automatically save your files as you type.",
-    href: "/",
-    cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" />,
-    className: "lg:row-start-1 lg:row-end-4 lg:col-start-2 lg:col-end-3",
-  },
-  {
-    name: "Full text search",
-    description: "Search through all your files in one place.",
-    href: "/",
-    cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" />,
-    className: "lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-3",
-  },
-  {
-    name: "Multilingual",
-    description: "Supports 100+ languages and counting.",
-    href: "/",
-    cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" />,
-    className: "lg:col-start-1 lg:col-end-2 lg:row-start-3 lg:row-end-4",
-  },
-  {
-    name: "Calendar",
-    description: "Use the calendar to filter your files by date.",
-    href: "/",
-    cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" />,
-    className: "lg:col-start-3 lg:col-end-3 lg:row-start-1 lg:row-end-2",
-  },
-  {
-    name: "Notifications",
-    description:
-      "Get notified when someone shares a file or mentions you in a comment.",
-    href: "/",
-    cta: "Learn more",
-    background: <img className="absolute -right-20 -top-20 opacity-60" />,
-    className: "lg:col-start-3 lg:col-end-3 lg:row-start-2 lg:row-end-4",
-  },
-];
 export default function Features() {
   return (
-    <>
-       <BentoGrid className="lg:grid-rows-3">
-      {features.map((feature) => (
-        <BentoCard key={feature?.name} name={feature?.name} description={feature?.description} href={feature?.href} cta={feature?.cta} background={feature?.background}  className={feature?.className} />
-      ))}
-    </BentoGrid>
+   <>
+   <div className={styles.header}>
+    <h1>Magical features to help you to<br/> Trade to next Level</h1>
+   </div>
+    <div className={styles.gridContainer}>
+      <div className={`${styles.gridItem} ${styles.item1}`}>
+        <img src="/grid0.svg" alt="Item 1" className={styles.gridImage} />
+      </div>
+      <div className={`${styles.gridItem} ${styles.item2}`}>
+        <img src="/grid2.svg" alt="Item 2" className={styles.gridImage} />
+      </div>
+      <div className={`${styles.gridItem} ${styles.item3}`}>
+        <img src="/grid4.svg" alt="Item 3" className={styles.gridImage} />
+      </div>
+      <div className={`${styles.gridItem} ${styles.item4}`}>
+        <img src="/grid5.svg" alt="Item 4" className={styles.gridImage} />
+      </div>
+      <div className={`${styles.gridItem} ${styles.item5}`}>
+        <img src="/grid8.svg" alt="Item 5" className={styles.gridImage} />
+      </div>
+    </div>
     </>
   );
 }
